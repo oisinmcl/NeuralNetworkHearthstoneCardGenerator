a74ad6f3db9b904e8525692b91b6c34fc1038758
oisinmcl <oisin_mcl_@hotmail.com>
2018-03-09 17:33:01 +0000

    Added Images
    Add logo's, background and card assets. Minor code changes

--- main_menu.py
+++ main_menu.py
@@ -1,14 +1,16 @@
 import kivy
-kivy.require('1.0.6')  # replace with your current kivy version !
+kivy.require('1.10.0')  # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.lang import Builder
+
 
 
 class Main_Menu(App):
-
+	
     def build(self):
-        return Button(text='Hello World')
+        return Builder.load_file('Screens/main_menu.kv')
 
 
 if __name__ == '__main__':
