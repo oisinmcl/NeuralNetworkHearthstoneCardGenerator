cf6611831e1ade86b235db3e7dcc390ea9cdeefc
oisinmcl <oisin_mcl_@hotmail.com>
2018-04-20 15:35:27 +0100

    Firebase Database functionality

--- database_manager.py
+++ database_manager.py
@@ -1,23 +1,35 @@
-
+from firebase import firebase
 
 class Database_Manager:
 
 	def __init__(self):
 		self.apiKey = ""
 		self.credentials = ""
-		self.dbURL = ""
+		self.dbURL = "https://final-year-project-f08b2.firebaseio.com/"
+		self.firebase = firebase.FirebaseApplication(self.dbURL, None)
 	
-	def get(self) 
-		pass
+	def get(self):
+		try:
+			result = self.firebase.get('/cards', None)
+			return result
+		except :
+			print('Error fetching data from firebase')
 		
-	def push(self) 
-		pass
-		
-	def delete(self) 
-		pass
+	def push(self, data):
+		try:
+			ref = self.firebase.post('/cards',data)
+		except :
+			print('Error pushing data to firebase')
+	
+	def put(self, id, data):
+		try:
+			self.firebase.put('/cards',id,data)		
+		except :
+			print('Error putting data to firebase')	
 
-	def set(self) 
-		pass
-
-	def update(self) 
-		pass		
+			
+	def delete(self, id):
+		try:		
+			self.firebase.delete('/cards', id)
+		except :
+			print('Error deleting data from firebase')			
--- temp.py
+++ temp.py
@@ -1,14 +1,27 @@
 from hs_rnn import Neural_Network 
 from hearthstoneAPI import HearthstoneAPI
+from database_manager import Database_Manager
+
+import json
 
 
+db =  Database_Manager()
 
-nn = Neural_Network()
+data = json.load(open('Output_data\output_1522685014.txt'))
+for cards in data:   
+	db.push(cards)
 
-api = HearthstoneAPI()
+	
+print(db.get())	
 
-api.getLastestSet()
-api.getCardsByParam('sets', 'Basic')
+db.delete('-LAYPe_GTk5zVX4lDvF_')
 
+db.put('-LAYPe_GTk5zVX4lDvF_', data[0])
+
+#api = HearthstoneAPI()
+#api.getLastestSet()
+#api.getCardsByParam('sets', 'Basic')
+
+#nn = Neural_Network()
 #nn.startTraining()
 #nn.StartGenerating(1000)
